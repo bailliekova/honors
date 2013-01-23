d0f18c78a0d86957d949bc30c36f119c44d42275
anov <novikova.go.zoom@gmail.com>
2013-01-22 21:42:41 -0600

    approval data and vote data, with scripts

--- getfavorablepolls.py
+++ getfavorablepolls.py
@@ -4,7 +4,7 @@ import csv
 baseurl='http://elections.huffingtonpost.com/pollster/api/polls.json'
 payload=dict()
 payload['state']='US'
-payload['chart']='obama-job-approval'
+payload['chart']='obama-favorable-rating'
 payload['page']=1
 payload['before']='2012-11-07'
 payload['after']='2012-06-01'
@@ -21,28 +21,18 @@ def process_favorable_responses(responses):
       print response['choice']
   return resdict
 
-def process_vote_responses(responses):
-  resdict={'other':0}
+def process_vote_responses(response):
+  resdict={}
   for response in responses:
       if 'obama' in response['choice'].lower():
         resdict['obama']=response['value']/100.0
       elif 'romney' in response['choice'].lower():
         resdict['romney']=response['value']/100.0
       else:
-        resdict['other']+=response['value'].lower()
-  return resdict
-
-def process_approval_responses(responses):
-  resdict={'approve':0, 'disapprove':0, 'other':0}
-  for response in responses:
-    if 'disapprove' in response['choice'].lower():
-      resdict['disapprove']+=response['value']/100.0
-    elif 'approve' in response['choice'].lower():
-      resdict['approve']+=response['value']/100.0
-    else:
-      print response['choice']
-      resdict['other']+=response['value']/100.0
-  return resdict
+        if 'other' in resdict:
+          resdict['other']+=response['value']/100.0
+        else:
+          resdict['other']==response['value']/100.0
 
 if __name__=='__main__':
   rows=[]
@@ -51,19 +41,19 @@ if __name__=='__main__':
   while data!=[]:
     for poll in data:
       for question in poll['questions']:
-        if question['chart']!='obama-job-approval':
+        if question['chart']!='obama-favorable-rating':
           continue
         for subpopulation in question['subpopulations']:
-          resdict=process_approval_responses(subpopulation['responses'])
+          resdict=process_favorable_responses(subpopulation['responses'])
           rows.append(
             [poll['start_date'], poll['end_date'], poll['pollster'], poll['method'], poll['source'], 
-            subpopulation['name'], resdict['approve'], resdict['disapprove'], resdict['other']]
+            subpopulation['name'], resdict['favorable'], resdict['unfavorable'], resdict['other']]
             )
     payload['page']+=1
     r=requests.get(baseurl, params=payload)
     data=r.json()
 
-  with open('approval_data.csv', 'wb') as csvfile:
+  with open('favorable_data.csv', 'wb') as csvfile:
     writer=csv.writer(csvfile, delimiter='\t')
     for row in rows:
       writer.writerow(row)
--- getvotepolls.py
+++ getvotepolls.py
@@ -4,7 +4,7 @@ import csv
 baseurl='http://elections.huffingtonpost.com/pollster/api/polls.json'
 payload=dict()
 payload['state']='US'
-payload['chart']='obama-job-approval'
+payload['chart']='2012-general-election-romney-vs-obama'
 payload['page']=1
 payload['before']='2012-11-07'
 payload['after']='2012-06-01'
@@ -29,19 +29,7 @@ def process_vote_responses(responses):
       elif 'romney' in response['choice'].lower():
         resdict['romney']=response['value']/100.0
       else:
-        resdict['other']+=response['value'].lower()
-  return resdict
-
-def process_approval_responses(responses):
-  resdict={'approve':0, 'disapprove':0, 'other':0}
-  for response in responses:
-    if 'disapprove' in response['choice'].lower():
-      resdict['disapprove']+=response['value']/100.0
-    elif 'approve' in response['choice'].lower():
-      resdict['approve']+=response['value']/100.0
-    else:
-      print response['choice']
-      resdict['other']+=response['value']/100.0
+          resdict['other']+=response['value']/100.0
   return resdict
 
 if __name__=='__main__':
@@ -51,19 +39,19 @@ if __name__=='__main__':
   while data!=[]:
     for poll in data:
       for question in poll['questions']:
-        if question['chart']!='obama-job-approval':
+        if question['chart']!='2012-general-election-romney-vs-obama':
           continue
         for subpopulation in question['subpopulations']:
-          resdict=process_approval_responses(subpopulation['responses'])
+          resdict=process_vote_responses(subpopulation['responses'])
           rows.append(
             [poll['start_date'], poll['end_date'], poll['pollster'], poll['method'], poll['source'], 
-            subpopulation['name'], resdict['approve'], resdict['disapprove'], resdict['other']]
+            subpopulation['name'], resdict['obama'], resdict['romney'], resdict['other']]
             )
     payload['page']+=1
     r=requests.get(baseurl, params=payload)
     data=r.json()
 
-  with open('approval_data.csv', 'wb') as csvfile:
+  with open('vote_data.csv', 'wb') as csvfile:
     writer=csv.writer(csvfile, delimiter='\t')
     for row in rows:
       writer.writerow(row)
