a3a9c6e360c800a5ae9a1f9f8b07d410c19400d3
anov <novikova.go.zoom@gmail.com>
2013-02-09 20:53:16 +0000

    fixed romney_cull

--- bin/romney_cull.py
+++ bin/romney_cull.py
@@ -5,5 +5,5 @@ outfile=codecs.open('data/romneytweets_culled.csv', 'w', encoding='utf-8')
 for line in infile:
 	line_1=line.lower()
 	tokens=line_1.split('\t')
-	if 'romney' or ' mitt 'in tokens[0]:
+	if ('romney' or ' mitt ') in tokens[0]:
 		outfile.write(line)
