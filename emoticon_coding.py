8725fe8efe239db05eea535b81bdacd5b2ac6f38
anov <novikova.go.zoom@gmail.com>
2013-02-12 18:10:44 -0600

    edits to R script, initial emoticon script

---
+++ emoticon_coding.py
@@ -0,0 +1,30 @@
+#script to grab emoticon data
+import codecs
+import sys
+import re
+
+
+if __name__ == '__main__':
+	infilename=sys.argv[1]
+	posre=re.compile(r'[:;B=]-?[\)\]}D]')
+	negre=re.compile(r'[:;B=]_?-?[\(\{\[]]')
+	trainingset=[]
+	with codecs.open(infilename, 'r', encoding='UTF-8') as infile:
+		for line in infile:
+			text=line.split('\t')[0]
+			m=posre.match(text)
+			if m:
+				trainingset.append((text, 'positive'))
+				try:
+					print text, 'positive'
+				except UnicodeError:
+					pass
+			m=negre.match(text)
+			if m:
+				trainingset.append((text, 'negative'))
+				try:
+					print text, 'negative'
+				except UnicodeError:
+					continue
+	print len(trainingset)
+		
--- script.R
+++ script.R
@@ -1,6 +1,7 @@
 #TODO: Learn R
-# my script
+#set working directory
 setwd("C:\\Users\\Anna\\Projects\\honors")
+#get zoo library for working with time series data
 require(zoo)
 
 #import sentiment data into a data frame
@@ -15,13 +16,17 @@ polldata[2]=as.Date(polldata[,2], format='%Y-%m-%d')
 sentimentdata[1]=as.Date(sentimentdata[,1], format='%Y-%m-%d')
 
 #merge into one data set, matching poll enddate with sentiment date, keeping all values from sentiment
-m=merge(polldata, sentimentdata, by.x="enddate", by.y="date", all=TRUE)
+m=merge(polldata, sentimentdata, by.x="enddate", by.y="date", all.y=TRUE)
 cor(m$sentiment, m$positive-m$negative)
 cor(m$favorable, m$positive)
 cor(m$unfavorable, m$negative)
 
 #compute moving average of sentiment data
 m$sentiment_ma=rollmean(m$sentiment, 3)
+
+#create columns with lag
+lag_num=2
+assign(paste("m$sentiment_ma_lagged_", as.character(lag_num)), lag(m$sentiment_ma, k=lag_num))
 
 #TODO: using zoo and lag functions(?) 
 #iterate through lags, calculating correlation coefficient, storing in a new dataframe to plot later.
